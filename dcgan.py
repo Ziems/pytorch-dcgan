74c4937cff0e9178357d0d067cdcfaa1d0b0c34e
Noah Ziems <nmziems@bsu.edu>
2019-04-04 16:01:02 -0400

    reafactor to cuda2

--- dcgan.py
+++ dcgan.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import torchvision
 from torchvision import transforms
